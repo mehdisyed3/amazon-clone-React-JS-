624d0b11648826d823b740ceaad50b80701ba177
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-01 19:20:57 -0500

    Login page css done. point 22-25 done - follow notes after point 27

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom'
 
 
 function Login() {
   return (
-    <div>
-      <h1>this is the LOgin page</h1>
+    <div className='login'>
+      <Link to='/'>
+        <img
+          className="login__logo"
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
+          alt=''
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input type='text' />
+
+          <h5>Password</h5>
+          <input type='password' />
+
+          <button className='login__signInButton' >Sign In</button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Noties
+          and our Interest-Based AdsNotice
+      </p>
+        <button className='login__registerButton'>Create your Amazon Account</button>
+      </div>
+
     </div>
   )
 }
