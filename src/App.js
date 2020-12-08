90653ef1474bb78e8455178f2695fde6698403d0
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-07 19:22:49 -0500

    Working on Payment Component, Signed up for Stripe, and initalised, refer to page title  for further instructions

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import Login from './Login'
 import { auth } from './firebase'
 import { Context } from './Context.js'
 import Payment from './Payment'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
 
 
-
-// t > 5.31
+const promise  = loadStripe('pk_test_51HvtWZDBgrMbvem70dLJ4lbMCSg9gWJcQJnOZZzfD6hV6HNFsMYoZ7Fv3lwJ60Ilm3uVqugOy1BbdzcPfpt6qe9i005SF4Sy4U')
+// t > 6.04
 function App() {
   const {setUserObj, setUser, user } = useContext(Context)
 
@@ -42,8 +44,11 @@ function App() {
           <Login />
         </Route>
         <Route path='/payment' >
+          <Elements stripe={promise}>
           <Payment />
+          </Elements>
         </Route>
+
 
       </Switch>
 
--- src/Payment.js
+++ src/Payment.js
@@ -1,63 +1,99 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './Payment.css'
 import { Context } from './Context'
 import CheckoutProduct from './CheckoutProduct'
 import { Link } from 'react-router-dom'
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import CurrencyFormat from 'react-currency-format'
 
 
-  function Payment() {
-    const { user, cart } = useContext(Context)
+function Payment() {
+  const { user, cart } = useContext(Context)
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true)
 
-    const items = cart.map((item, i) => <CheckoutProduct key={i} id={item.id} image={item.image} title={item.title} price={item.price} rating={item.rating} />)
+  const items = cart.map((item, i) => <CheckoutProduct key={i} id={item.id} image={item.image} title={item.title} price={item.price} rating={item.rating} />)
 
+  const stripe = useStripe()
+  const element = useElements()
 
-    return (
-      <div className='payment'>
-        <div className='payment__container'>
-          <h1>
-            Checkout (
+  const handleSubmit = (e) => {
+    console.log("Dabba")
+  }
+
+  const handleChange = event => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  }
+
+  const total = cart?.length && cart.reduce((acc, cv) => acc + cv.price, 0)
+
+  return (
+    <div className='payment'>
+      <div className='payment__container'>
+        <h1>
+          Checkout (
                         <Link to="/checkout">{cart?.length} items</Link>
                         )
                 </h1>
-          <div className='payment__section'>
-            <div className='payment__title'>
-              <h3>Delivery Address</h3>
-
-            </div>
-            <div className='payment__address'>
-              <p>{user}</p>
-              <p>12 Aylesbury Dr</p>
-              <p>Brampton, ON</p>
-            </div>
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Delivery Address</h3>
 
           </div>
-
-          <div className='payment__section'>
-            <div className='payment__title'>
-              <h3>Review items and delivery</h3>
-            </div>
-            <div className='payment__items'>
-
-              {items}
-
-            </div>
-          </div>
-
-          <div className='payment__section'>
-            <div className='payment__title'>
-              <h3>Payment Method</h3>
-            </div>
-            <div className='payment__details'>
-              {/* Stripe magic */}
-
-            </div>
-
-
+          <div className='payment__address'>
+            <p>{user}</p>
+            <p>12 Aylesbury Dr</p>
+            <p>Brampton, ON</p>
           </div>
 
         </div>
+
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className='payment__items'>
+
+            {items}
+
+          </div>
+        </div>
+
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Payment Method</h3>
+          </div>
+          <div className='payment__details'>
+            {/* Stripe magic */}
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className='payment__priceContainer'>
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <h3>Order Total: {value}</h3>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={total}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+              </div>
+            </form>
+
+          </div>
+
+
+        </div>
+
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export default Payment
