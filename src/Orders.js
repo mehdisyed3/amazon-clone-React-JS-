ef4007c77c25ed2d66c259c379b4fc289680e67e
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-08 21:07:14 -0500

    working on order.js,

--- src/Orders.js
+++ src/Orders.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Orders.css'
+import { db } from './firebase'
+import { Context } from './Context'
 
 function Orders() {
+  const [orders, setOrders] = useState([])
+  const { userObj, cart } = useContext(Context)
+
+  useEffect(() => {
+    if (userObj) {
+      db
+        .collection('users')
+        .doc(userObj?.uid)
+        .collection('orders')
+        .orderBy('created', 'desc')
+        .onSnapshot(snapshot => (
+          setOrders(snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data()
+          })))
+        ))
+    } else {
+      setOrders([])
+    }
+
+  }, [userObj])
   return (
     <div className='orders'>
       <h1>Your Orders</h1>
-      
+
+      {/* <div className='orders__order'>
+                {orders?.map(order => (
+                    <Order order={order} />
+                ))}
+            </div> */}
+
     </div>
   )
 }
