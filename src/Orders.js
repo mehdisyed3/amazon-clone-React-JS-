aa06dd83a106ae183470effd55ab1171dd8e3110
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-08 21:43:06 -0500

    working on Orders.js

--- src/Orders.js
+++ src/Orders.js
@@ -8,14 +8,14 @@ function Orders() {
   const { userObj, cart } = useContext(Context)
 
   useEffect(() => {
-    if (userObj) {
+    if (userObj) {  
       db
-        .collection('users')
-        .doc(userObj?.uid)
-        .collection('orders')
-        .orderBy('created', 'desc')
+        .collection('users') // accessing users collection << 7.47 vdo par >>
+        .doc(userObj?.uid) // getting the specific user that is logged in
+        .collection('orders') // accessing the specific users order
+        .orderBy('created', 'desc') // ordering bsed on date created in desending order, (most recent one should be on the top)
         .onSnapshot(snapshot => (
-          setOrders(snapshot.docs.map(doc => ({
+          setOrders(snapshot.docs.map(doc => ({ // map through all the orders and shows it 
             id: doc.id,
             data: doc.data()
           })))
@@ -28,8 +28,8 @@ function Orders() {
   return (
     <div className='orders'>
       <h1>Your Orders</h1>
-
-      {/* <div className='orders__order'>
+{/* 
+      <div className='orders__order'>
                 {orders?.map(order => (
                     <Order order={order} />
                 ))}
